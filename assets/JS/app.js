0bfda0b3094d90ea00f13db707a1cd8dd4e1beb9
abbasoov <ismayiliib@code.edu.az>
2022-07-08 00:02:05 +0400

    modified

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -12,7 +12,7 @@ function Delete(event){
 
 addBtn.onclick = ()=>{
     var input=document.getElementById("input").value;
-    li=`<li><span class="span" id="content">${input}</span> <input id="checkbox" type="checkbox"><button id="del" class="btn btn-danger" onclick="Delete(event)">Delete</button></li>`
+    li=`<li><span class="span" id="content">${input}</span><button id="del" class="btn btn-danger" onclick="Delete(event)">Delete</button></li>`
     document.getElementById('ul').innerHTML +=li
   
 
@@ -20,39 +20,9 @@ addBtn.onclick = ()=>{
 }
 
 
-    
-// function ticked(event){
-//     document.getElementById('content').style.textdecoration ='line-through'
-
-// }
-
-var ul = document.querySelector('ul');
 ul.addEventListener('click', function(event) {
   if (event.target.tagName === 'LI') {
     event.target.classList.toggle('check');
   }
 }, false);
 
-// chkbox.addEventListener('click',(event)=>{
-//     if (event.target.tagName === 'input') {
-//         event.target.classList.toggle('check');
-//       }
-//     }, false
-// )
-
-// addBtn.addEventListener("click",function() {
-//  li=`<li>${input.value} <div class="btn btn-danger">Delete</div></li>`
-//  ul.insertAdjacentHTML("beforeend",li)
-//  document.getElementById("input").value=""
-//  Getbtn()
-// })
-
-// function Getbtn() {
-// document.querySelectorAll(".btn").forEach(x=>{
-//    x.addEventListener("click",function (e) {
-//     
-//    })
-//    li=ul.innerHTML
-  
-// })
-// }
